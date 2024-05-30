9aa51eaaa9673ded938d7851a38d11e804b4c325
Chandrika3335 <163969029+Chandrika3335@users.noreply.github.com>
2024-05-30 22:48:45 +0530

    Update general.service.ts

--- revhire/src/app/Service/general.service.ts
+++ revhire/src/app/Service/general.service.ts
@@ -9,7 +9,7 @@ import { LoginCredentials } from '../Model/login-credentials';
 })
 export class GeneralService {
 
-  base_url = "http://20.242.236.34:8082/";
+  base_url = "http://4.157.23.251:8082/";
   constructor(private http: HttpClient) {   }
 
   public getUser(userId:number):Observable <any>{
