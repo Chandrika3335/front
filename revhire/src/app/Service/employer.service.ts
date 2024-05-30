c49288a50c2bfcb7683165ead379472eda82cbd4
Chandrika3335 <163969029+Chandrika3335@users.noreply.github.com>
2024-05-30 23:32:48 +0530

    Update employer.service.ts

--- revhire/src/app/Service/employer.service.ts
+++ revhire/src/app/Service/employer.service.ts
@@ -9,7 +9,7 @@ import { Application } from '../Model/application';
 })
 export class EmployerService {
 
-  base_url = "http://4.157.23.251:8082/";
+  base_url = "http://172.176.55.253:8082/";
   constructor(private http: HttpClient) {   }
 
   public getJob(Id:number):Observable <any>{
